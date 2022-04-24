874a30397b8e3c563cb5380050add838e0b6873c
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2022-04-23 23:55:17 -0400

    first commit

---
+++ src/test/java/com/talentyco/java/JavaMultimoduloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentyco.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMultimoduloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
