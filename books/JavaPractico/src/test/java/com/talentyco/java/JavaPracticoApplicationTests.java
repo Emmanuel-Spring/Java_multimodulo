42da8e18ceef33c3277b328a73819b0bacfc60ee
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2022-04-24 00:26:53 -0400

    first commit

---
+++ books/JavaPractico/src/test/java/com/talentyco/java/JavaPracticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentyco.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
